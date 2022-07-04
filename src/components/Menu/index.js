47cde5d3ddcda4bce2fd338da1db4b6583143ff1
darnell422 <92842510+darnell422@users.noreply.github.com>
2022-07-03 19:57:50 -0500

    B

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -7,16 +7,16 @@ const Menu =()=>{
         <div className='menu'>
             <div className = 'nav' >
                 <NavLink exact='true' activeclassname="active" to='/'>
-                    <h1>Home</h1>
+                    <h1>HOME</h1>
                 </NavLink>
                 <NavLink activeclassname="active" to='/about'>
-                    <h1>About</h1>
+                    <h1>ABOUT</h1>
                 </NavLink>
                 <NavLink activeClassName="active" to='home'>
-                    <h1>Home</h1>
+                    <h1>CONTACT</h1>
                 </NavLink>
                 <NavLink activeClassName="active" to='home'>
-                    <h1>Home</h1>
+                    <h1>PROJECTS</h1>
                 </NavLink>
             </div>
         </div>
